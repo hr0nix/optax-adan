05c65c458f0dfc89ccc1d2c8a5cffb33ada865da
hr0nix <boris.jangel@gmail.com>
2022-08-23 17:35:27 +0300

    Added setup.py, bumped version to 0.1.1

--- optax_adan/__init__.py
+++ optax_adan/__init__.py
@@ -1,1 +1,5 @@
+__version__ = '0.1.1'
+__author__ = 'Boris Yangel'
+
 from .transform import adan
+
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='optax-adan',
+    version='0.1.1',
+    description='An implementation of adan optimization algorithm for optax.',
+    url='https://github.com/hr0nix/optax-adan',
+    author='Boris Yangel',
+    author_email='boris.jangel@gmail.com',
+    license='Apache License 2.0',
+    packages=['optax_adan'],
+    install_requires=['optax'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+    ],
+)
